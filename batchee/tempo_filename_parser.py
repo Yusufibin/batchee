f2dc6b902163a7a0cac73ee972d32f015c9fb8a0
Youssouf ⚜️ <71589435+Yusufibin@users.noreply.github.com>
2025-12-09 22:23:43 +0100

    Add comment for day_and_scans list clarification
    Added comment to clarify the purpose of the day_and_scans list.

--- batchee/tempo_filename_parser.py
+++ batchee/tempo_filename_parser.py
@@ -89,7 +89,9 @@ def get_batch_indices(filenames: list, logger: logging.Logger = default_logger) 
     """
 
     logger.info(f"get_batch_indices() starting --- with {len(filenames)} filenames")
-
+    
+    # Make a new list with days and scans, e.g. [('20130701', 'S009'), ('20130701', 'S009'), ...]
+    
     day_and_scans: list[tuple[str, str]] = []
     for name in filenames:
         matches = tempo_granule_filename_pattern.match(name)
